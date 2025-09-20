a5077150a4566c7507ecf23f4c4fc732bea8a012
riccardo-guido <guidoriccardo02@gmail.com>
2025-09-20 12:17:23 +0200

    snack 2

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,21 @@ function controlDataType() {
 }
 let data: unknown = null;
 controlDataType();
+
+// SNACK 2
+
+type Dipendente = {
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: "m" | "f";
+  anniDiServizio: number[];
+};
+
+let Roberto: Dipendente = {
+  nome: "Roberto",
+  cognome: "Rossi",
+  annoNascita: 1980,
+  sesso: "m",
+  anniDiServizio: [2010, 2011, 2012, 2013, 2014, 2015],
+};
